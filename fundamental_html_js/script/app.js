d953dd6f67f420e24fc1a8cd86879d3e4cbdba2e
Royhan <royhankeren12@gmail.com>
2024-06-08 21:13:53 +0700

    object and classes

--- fundamental_html_js/script/app.js
+++ fundamental_html_js/script/app.js
@@ -1,3 +1,4 @@
+
 // jika ingin membuat sebuah variabel menggunakan let
 // let a = 5;
 // a = 10;
@@ -7,12 +8,65 @@
 // a = 10; <== ERRROR (const tidak bisa di ubah, atau nilai tetap)
 
 
-// Penggunaan function
-function greet() {
-    console.log("Hello Wolrd");
+ // Penggunaan function
+// function greet(
+//   userName,
+//   message /*PENGGUNAAN PARAMETER DI FUNCTION JAVASCRIPT*/
+// ) {
+//   // console.log(userName);
+//   // console.log(message);
+
+//   // PENGGUNAAN RETURN
+//   return "Hello My name is " + userName + ". " + message;
+// }
+
+// console.log(greet("Roy ", "Hello Royhan"));
+// console.time("myTimer");
+
+
+/**
+ * ARROW FUNCTION 
+ * 
+ * const greet = (userName,message) => {
+ *   return userName + message;
+ * }
+ * 
+ * // ATAU 
+ * 
+ * const greet = (THIS_PARAMETER) => {THIS_FOR_COMPONENT}
+ */
+
+/**
+ * OBJECT AND CLASS
+ * 
+ * 
+ */
+
+const greet = (userName,message) => {
+    return userName + message;
 }
 
+const user = {
+    name: 'Rizki',
+    age: 15,
 
-greet();
+    greet() {
+        console.log(this.name);
+    }
+};
 
-console.time("myTimer");
+const people = {
+    name: "Royhan",
+    age: 20,
+    jurusan: "Teknik Informatika"
+}
+
+console.log(people);
+console.log(people.name);
+console.log(people.jurusan);
+
+console.log(user);
+console.log(user.age);
+
+const userName = "Max";
+const userAge = 35;
