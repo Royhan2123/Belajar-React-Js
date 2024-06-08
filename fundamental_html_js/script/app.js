c04cd4230c4ba92339175d4899ff10ba01128c1f
Royhan <royhankeren12@gmail.com>
2024-06-08 19:55:59 +0700

    add function

--- fundamental_html_js/script/app.js
+++ fundamental_html_js/script/app.js
@@ -1,6 +1,18 @@
-// import { apiKey } from "./utils";
-// import apiKey from "./utils";
+// jika ingin membuat sebuah variabel menggunakan let
+// let a = 5;
+// a = 10;
+
+// jika ingin menggunakan val seperti di kotlin harus menggunakan const seperti dart
+// const a = 5;
+// a = 10; <== ERRROR (const tidak bisa di ubah, atau nilai tetap)
 
 
-console.log("Hello World")
+// Penggunaan function
+function greet() {
+    console.log("Hello Wolrd");
+}
 
+
+greet();
+
+console.time("myTimer");
