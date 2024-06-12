2dd1aef717cc73ff15f1e6250077d1c25bde274f
Royhan <royhankeren12@gmail.com>
2024-06-12 23:33:37 +0700

    show var in html react

--- belajar-react-jsx/src/App.js
+++ belajar-react-jsx/src/App.js
@@ -1,27 +1,42 @@
-import './App.css';
+import "./App.css";
 
-function Header(){
-  return <>
-    <header>
-        <img src="/assets/react-core-concepts.png" alt="Stylized atom" className="cover-image" />
+const iniArray = ["Fundamental ", "React ", "Core "];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+// cara menampilkan variabel ke dalam html dengan cara menggunakan kurung kurawal {};
+// const name = "Hello My name is : ";
+// {nama_variabel} --> contoh : <p> {name} Royhan <p/>  
+
+function Header() {
+  return (
+    <>
+      <header>
+        <img
+          src="/assets/react-core-concepts.png"
+          alt="Stylized atom"
+          className="cover-image"
+        />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
+          {iniArray[getRandomInt(2)]} React concepts you will need for almost any app you
+          are going to build!
         </p>
       </header>
-  </>
+    </>
+  );
 }
 
 function App() {
-  return <>
+  return (
+    <>
       <div className="App">
-          <Header/>
-          <main>
-            Hello
-          </main>
+        <Header />
       </div>
-  </>
+    </>
+  );
 }
 
 export default App;
