126c1fb6be87b4153fb14ec12f97c7f5901a65fb
Royhan <royhankeren12@gmail.com>
2024-06-13 00:15:54 +0700

    add section and add dynamis image in html

--- belajar-react-jsx/src/App.js
+++ belajar-react-jsx/src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
-import reactImg from './assets/react-core-concepts.png';
+import reactImg from "./assets/react-core-concepts.png";
+import componentImg from "./assets/components.png";
+import configImg from "./assets/config.png";
+import jsxUiImg from "./assets/jsx-ui.png";
+import stateMgmtImg from "./assets/state-mgmt.png";
 
 const iniArray = ["Fundamental ", "React ", "Core "];
 
@@ -16,11 +20,7 @@ function Header() {
   return (
     <>
       <header>
-        <img
-          src= {reactImg}
-          alt="Stylized atom"
-          className="cover-image"
-        />
+        <img src={reactImg} alt="Stylized atom" className="cover-image" />
         <h1>React Essentials</h1>
         <p>
           {description} React concepts you will need for almost any app you are
@@ -36,9 +36,49 @@ function App() {
     <>
       <div className="App">
         <Header />
+        <main>
+          <section id="core-concepts">
+            <h2>Core Concepts</h2>
+            <br/>
+            <ul>
+              <CoreConcept
+                title="Components"
+                description="The core UI building block."
+                img={componentImg}
+              />
+              <CoreConcept
+                title="Config"
+                description="Config Image "
+                img={configImg}
+              />
+              <CoreConcept
+                title="Jsx Ui"
+                description="Jsx UI Image"
+                img={jsxUiImg}
+              />
+              <CoreConcept
+                title="State "
+                description="Image State"
+                img={stateMgmtImg}
+              />
+            </ul>
+          </section>
+        </main>
       </div>
-    </> 
+    </>
   );
 }
+
+const CoreConcept = (props) => {
+  return (
+    <>
+      <li>
+        <img src={props.img} alt={props.title} />
+        <h3>{props.title}</h3>
+        <p> {props.description} </p>
+      </li>
+    </>
+  );
+};
 
 export default App;
