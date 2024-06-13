d3c12c46c2c5491a6634957a77117cce6260acd5
Royhan <royhankeren12@gmail.com>
2024-06-13 23:42:17 +0700

    import file Header.js

--- belajar-react-jsx/src/App.js
+++ belajar-react-jsx/src/App.js
@@ -1,63 +1,49 @@
 import "./App.css";
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPST } from "./data";
+import Header from "./components/headher";
+/**
+ * Disini kita akan menggabungkan semua data dan langsung memanggil nya seperti di bawah ini saja
+ * yang dimana sebelumnya --> :
+ 
+  <CoreConcept image={CORE_CONCEPST[0].image} title={CORE_CONCEPST[0].title} 
+  description={CORE_CONCEPST[0].description}/>
 
-const iniArray = ["Fundamental ", "React ", "Core "];
+  menjadi : 
+  CoreConcept {...CORE_CONCEPST[0]} />   <--- disini kita menggunakan operator spreat
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
 
-// cara menampilkan variabel ke dalam html dengan cara menggunakan kurung kurawal {};
-// const name = "Hello My name is : ";
-// {nama_variabel} --> contoh : <p> {name} Royhan <p/>
-
-function Header() {
-  const description = iniArray[getRandomInt(2)];
+  yang dimana function CoreConcept nya seperti ini : 
+  const CoreConcept = ({image,title,description}) => {
   return (
     <>
-      <header>
-        <img src={reactImg} alt="Stylized atom" className="cover-image" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <li>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p> {description} </p>
+      </li>
     </>
   );
-}
+};
 
+
+ * sebelumnya kita membuatnya dengan props, tetapi disini kita langsung memasukkan var nya seperti
+ * contoh di atas 
+ */
 function App() {
   return (
     <>
       <div className="App">
-        <Header />
+        <Header/>
         <main>
           <section id="core-concepts">
             <h2>Core Concepts</h2>
-            <br/>
+            <br />
             <ul>
-              <CoreConcept
-                title={CORE_CONCEPST[0].title}
-                description={CORE_CONCEPST[0].description}
-                img={CORE_CONCEPST[0].image}
-              />
-              <CoreConcept
-                 title={CORE_CONCEPST[1].title}
-                 description={CORE_CONCEPST[1].description}
-                 img={CORE_CONCEPST[1].image}
-              />
-              <CoreConcept
-                 title={CORE_CONCEPST[2].title}
-                 description={CORE_CONCEPST[2].description}
-                 img={CORE_CONCEPST[2].image}
-              />
-              <CoreConcept
-                title={CORE_CONCEPST[3].title}
-                description={CORE_CONCEPST[3].description}
-                img={CORE_CONCEPST[3].image}
-              />
+              
+              <CoreConcept {...CORE_CONCEPST[0]} />
+              <CoreConcept {...CORE_CONCEPST[1]} />
+              <CoreConcept {...CORE_CONCEPST[2]} />
+              <CoreConcept {...CORE_CONCEPST[3]} />
             </ul>
           </section>
         </main>
@@ -68,26 +54,26 @@ function App() {
 
 // Disini peran props digunakan sebagai parameter tanpa ada nya tipe data
 // yang dimana disini dia harus membutuhkan anak sebagai inang nya
-// contoh nya seperti : --> 
+// contoh nya seperti : -->
 /**
  * cosnt User = (props) => {
  * <img src= {props.img} /> <-- untuk nama nya bebas ya sesuai dengan yang di inginkan
  * <p>{props.title}<p/>
  * }
- * 
- * atau bisa langsung seperti ini : 
+ *
+ * atau bisa langsung seperti ini :
  * cosnt User = ({img, title}) => {
- * <img src= {img} /> 
+ * <img src= {img} />
  * <p>{title}<p/>
  * }
  */
-const CoreConcept = (props) => {
+const CoreConcept = ({image,title,description}) => {
   return (
     <>
       <li>
-        <img src={props.img} alt={props.title} />
-        <h3>{props.title}</h3>
-        <p> {props.description} </p>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p> {description} </p>
       </li>
     </>
   );
---
+++ belajar-react-jsx/src/components/headher.js
@@ -0,0 +1,30 @@
+import reactImg from "../assets/react-core-concepts.png";
+import "../components/style_header.css";
+
+const iniArray = ["Fundamental ", "React ", "Core "];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+// cara menampilkan variabel ke dalam html dengan cara menggunakan kurung kurawal {};
+// const name = "Hello My name is : ";
+// {nama_variabel} --> contoh : <p> {name} Royhan <p/>
+
+const Header = () => {
+  const description = iniArray[getRandomInt(2)];
+  return (
+    <>
+      <header>
+        <img src={reactImg} alt="Stylized atom" className="cover-image" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    </>
+  );
+};
+
+export default Header;
